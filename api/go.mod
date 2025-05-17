cc4cf11128b5b1a0a2436dbe2545b7f7774b5904
mhmmdihza <mhmmdihza@gmail.com>
2025-05-18 01:12:27 +0700

    install godotenv for easier hybrid config  between the .env file and os.Getenv

--- api/go.mod
+++ api/go.mod
@@ -4,6 +4,7 @@ go 1.24.2
 
 require (
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/pressly/goose v2.7.0+incompatible
 	github.com/sirupsen/logrus v1.9.3
--- api/main.go
+++ api/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"log"
+	"os"
 	"payd/util"
 
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	util.InitLogger(logrus.DebugLevel)
-
-	log := util.Log()
-	log.Info("starting...")
+	initLog()
+	logrus := util.Log()
+	logrus.Debug("debug mode")
+	logrus.Info("starting...")
+}
+func initLog() {
+	logLevelStr := os.Getenv("LOG_LEVEL")
+	logLevel, err := logrus.ParseLevel(os.Getenv("LOG_LEVEL"))
+	if err != nil {
+		log.Printf("unknown log level :'%s' , fallback to the info level", logLevelStr)
+		logLevel = logrus.InfoLevel
+	}
+	util.InitLogger(logLevel)
 }
